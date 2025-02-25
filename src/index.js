f743bdcaab147aee812f484e5f7e2a97441961d5
devovi2024 <arfanhosenovi@gmail.com>
2025-02-25 22:53:14 +0600

    adding functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const allTabsBody = document.querySelectorAll('.tab-body-single');
 const allTabsHead = document.querySelectorAll('.tab-head-single');
 const searchForm = document.querySelector('.app-header-search');
-let searchList = document.getElementById('search-list');
+const searchList = document.getElementById('search-list');
 
 let activeTab = 1, allData;
 
@@ -10,23 +10,25 @@ const init = () => {
     showActiveTabHead();
 }
 
-const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
+const showActiveTabHead = () => {
+    hideAllTabHead();
+    allTabsHead[activeTab - 1].classList.add('active-tab');
+}
 
 const showActiveTabBody = () => {
     hideAllTabBody();
     allTabsBody[activeTab - 1].classList.add('show-tab');
 }
 
-const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
-const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
+const hideAllTabBody = () => allTabsBody.forEach(tabBody => tabBody.classList.remove('show-tab'));
+const hideAllTabHead = () => allTabsHead.forEach(tabHead => tabHead.classList.remove('active-tab'));
 
-// even listeners
-window.addEventListener('DOMContentLoaded', () => init());
-// button event listeners
-allTabsHead.forEach(singleTabHead => {
-    singleTabHead.addEventListener('click', () => {
-        hideAllTabHead();
-        activeTab = singleTabHead.dataset.id;
+// Event listeners
+window.addEventListener('DOMContentLoaded', init);
+
+allTabsHead.forEach(tabHead => {
+    tabHead.addEventListener('click', () => {
+        activeTab = tabHead.dataset.id;
         showActiveTabHead();
         showActiveTabBody();
     });
@@ -34,184 +36,77 @@ allTabsHead.forEach(singleTabHead => {
 
 const getInputValue = (event) => {
     event.preventDefault();
-    let searchText = searchForm.search.value;
+    const searchText = searchForm.search.value;
     fetchAllSuperHero(searchText);
 }
 
-// search form submission
+// Search form submission
 searchForm.addEventListener('submit', getInputValue);
 
-// api key => 727054372039115
-const fetchAllSuperHero = async(searchText) => {
-    let url = `https://www.superheroapi.com/api.php/727054372039115/search/${searchText}`;
-    try{
+const fetchAllSuperHero = async (searchText) => {
+    const url = `https://www.superheroapi.com/api.php/727054372039115/search/${searchText}`;
+    try {
         const response = await fetch(url);
         allData = await response.json();
-        if(allData.response === 'success'){
-            // console.log(allData);
+        if (allData.response === 'success') {
             showSearchList(allData.results);
         }
-    } catch(error){
-        console.log(error);
+    } catch (error) {
+        console.error(error);
     }
 }
 
 const showSearchList = (data) => {
     searchList.innerHTML = "";
-    data.forEach(dataItem => {
+    data.forEach(({ id, name, image }) => {
         const divElem = document.createElement('div');
         divElem.classList.add('search-list-item');
         divElem.innerHTML = `
-            <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt = "">
-            <p data-id = "${dataItem.id}">${dataItem.name}</p>
+            <img src="${image.url || ''}" alt="">
+            <p data-id="${id}">${name}</p>
         `;
         searchList.appendChild(divElem);
     });
 }
 
 searchForm.search.addEventListener('keyup', () => {
-    if(searchForm.search.value.length > 1){
-        fetchAllSuperHero(searchForm.search.value);
+    const searchValue = searchForm.search.value;
+    if (searchValue.length > 1) {
+        fetchAllSuperHero(searchValue);
     } else {
         searchList.innerHTML = "";
     }
 });
 
 searchList.addEventListener('click', (event) => {
-    let searchId = event.target.dataset.id;
-    let singleData = allData.results.filter(singleData => {
-        return searchId === singleData.id;
-    })
-    showSuperheroDetails(singleData);
-    searchList.innerHTML = "";
+    const searchId = event.target.dataset.id;
+    const singleData = allData.results.find(dataItem => searchId === dataItem.id);
+    if (singleData) {
+        showSuperheroDetails(singleData);
+        searchList.innerHTML = "";
+    }
 });
 
 const showSuperheroDetails = (data) => {
-    console.log(data);
-    document.querySelector('.app-body-content-thumbnail').innerHTML = `
-        <img src = "${data[0].image.url}">
-    `;
+    const { image, name, powerstats, biography, appearance, connections } = data;
 
-    document.querySelector('.name').textContent = data[0].name;
-    document.querySelector('.powerstats').innerHTML = `
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield-halved"></i>
-            <span>intelligence</span>
-        </div>
-        <span>${data[0].powerstats.intelligence}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield-halved"></i>
-            <span>strength</span>
-        </div>
-        <span>${data[0].powerstats.strength}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield-halved"></i>
-            <span>speed</span>
-        </div>
-        <span>${data[0].powerstats.speed}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield-halved"></i>
-            <span>durability</span>
-        </div>
-        <span>${data[0].powerstats.durability}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield-halved"></i>
-            <span>power</span>
-        </div>
-        <span>${data[0].powerstats.power}</span>
-    </li>
-    <li>
-        <div>
-            <i class = "fa-solid fa-shield-halved"></i>
-            <span>combat</span>
-        </div>
-        <span>${data[0].powerstats.combat}</span>
-    </li>
-    `;
+    document.querySelector('.app-body-content-thumbnail').innerHTML = `<img src="${image.url}">`;
+    document.querySelector('.name').textContent = name;
 
-    document.querySelector('.biography').innerHTML = `
-    <li>
-        <span>full name</span>
-        <span>${data[0].biography['full-name']}</span>
-    </li>
-    <li>
-        <span>alert-egos</span>
-        <span>${data[0].biography['alter-egos']}</span>
-    </li>
-    <li>
-        <span>aliases</span>
-        <span>${data[0].biography['aliases']}</span>
-    </li>
-    <li>
-        <span>place-of-birth</span>
-        <span>${data[0].biography['place-of-birth']}</span>
-    </li>
-    <li>
-        <span>first-apperance</span>
-        <span>${data[0].biography['first-appearance']}</span>
-    </li>
-    <li>
-        <span>publisher</span>
-        <span>${data[0].biography['publisher']}</span>
-    </li>
-    `;
+    const createStatsList = (stats) => {
+        return Object.entries(stats).map(([key, value]) => `
+            <li>
+                <div>
+                    <i class="fa-solid fa-shield-halved"></i>
+                    <span>${key}</span>
+                </div>
+                <span>${value}</span>
+            </li>
+        `).join('');
+    };
 
-    document.querySelector('.appearance').innerHTML = `
-    <li>
-        <span>
-            <i class = "fas fa-star"></i> gender
-        </span>
-        <span>${data[0].appearance['gender']}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fas fa-star"></i> race
-        </span>
-        <span>${data[0].appearance['race']}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fas fa-star"></i> height
-        </span>
-        <span>${data[0].appearance['height'][0]}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fas fa-star"></i> weight
-        </span>
-        <span>${data[0].appearance['weight'][0]}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fas fa-star"></i> eye-color
-        </span>
-        <span>${data[0].appearance['eye-color']}</span>
-    </li>
-    <li>
-        <span>
-            <i class = "fas fa-star"></i> hair-color
-        </span>
-        <span>${data[0].appearance['hair-color']}</span>
-    </li>
-    `;
-
-    document.querySelector('.connections').innerHTML = `
-    <li>
-        <span>group--affiliation</span>
-        <span>${data[0].connections['group-affiliation']}</span>
-    </li>
-    <li>
-        <span>relatives</span>
-        <span>${data[0].connections['relatives']}</span>
-    </li>
-    `;
+    document.querySelector('.powerstats').innerHTML = createStatsList(powerstats);
+    document.querySelector('.biography').innerHTML = createStatsList(biography);
+    document.querySelector('.appearance').innerHTML = createStatsList(appearance);
+    document.querySelector('.connections').innerHTML = createStatsList(connections);
 }
